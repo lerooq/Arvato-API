479c24140505838ef6b7ae2f31d999f6db3b5934
emry <emry@itu.dk>
2021-10-05 13:06:16 +0200

    cleanup + postman check

--- Arvato-API-Task.Models/Helpers/Extensions.cs
+++ Arvato-API-Task.Models/Helpers/Extensions.cs
@@ -6,20 +6,5 @@ namespace Arvato_API_Task.Models.Helpers
 {
     public static class Extensions
     {
-        public static string ConcatWithDelimeter(this ICollection<string> arr, char delimeter)
-        {
-            StringBuilder sb = new StringBuilder();
-            int index = 0;
-            foreach (var word in arr)
-            {
-                if (string.IsNullOrEmpty(word))
-                    continue;
-                sb.Append(word);
-                if (index < arr.Count - 1) sb.Append(delimeter);
-
-                index++;
-            }
-            return sb.ToString();
-        }
     }
 }
